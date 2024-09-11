c64d4538edfd1cdd2e76c4e9d63b9c7e148387bf
Huascar <huascarfedor@gmail.com>
2024-09-11 11:24:16 -0400

    segundo commit

--- src/calculadora/calculadora.controller.spec.ts
+++ src/calculadora/calculadora.controller.spec.ts
@@ -1,18 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CalculadoraController } from './calculadora.controller';
+import { CalculadoraService } from './calculadora.service';
 
 describe('CalculadoraController', () => {
   let controller: CalculadoraController;
+  let service: CalculadoraService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CalculadoraController],
+      providers: [CalculadoraService],
     }).compile();
 
     controller = module.get<CalculadoraController>(CalculadoraController);
+    service = module.get<CalculadoraService>(CalculadoraService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('Debería sumar dos numeros', () => {
+    jest.spyOn(service, 'sumar').mockImplementation(() => 10);
+    expect(controller.sumar(4, 6)).toBe(10);
+  });
+
+  it('Deberia lanzar error al sumar numeros negativos', () => {
+    jest.spyOn(service, 'sumar').mockImplementation(() => {
+      throw new Error();
+    });
+    expect(() => controller.sumar(-1, 8)).toThrow(Error);
+  });
+
+  it('Deberia lanzar error al sumar numeros negativos', () => {
+    jest.spyOn(service, 'sumar').mockImplementation(() => {
+      throw new Error();
+    });
+    expect(() => controller.sumar(1, -8)).toThrow(Error);
   });
 });
--- src/calculadora/calculadora.controller.ts
+++ src/calculadora/calculadora.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, ParseIntPipe, Query } from '@nestjs/common';
+import { CalculadoraService } from './calculadora.service';
 
 @Controller('calculadora')
-export class CalculadoraController {}
+export class CalculadoraController {
+  constructor(private readonly calculadoraService: CalculadoraService) {}
+
+  @Get('sumar')
+  sumar(
+    @Query('sumando1', ParseIntPipe) sumando1: number,
+    @Query('sumando2', ParseIntPipe) sumando2: number,
+  ): number {
+    return this.calculadoraService.sumar(sumando1, sumando2);
+  }
+}
